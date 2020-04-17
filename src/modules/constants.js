10382186ff4b891d2f04419cd7fc487abe49f4c1
eligarlo <eligarlo.developer@gmail.com>
2020-04-17 14:54:57 +0300

    created constants module

---
+++ src/modules/constants.js
@@ -0,0 +1,22 @@
+class Constants {
+	ApiUrl() {
+		return {
+			posts: 'http://localhost:3000/posts'
+		}
+	}
+
+	DOMSelectors() {
+		return {
+			alert: document.querySelector('.alert'),
+			postForm: document.querySelector('.postsContainer'),
+			cardForm: document.querySelector('.card-form'),
+			post: document.querySelector('#posts'),
+			titleInput: document.querySelector('#title'),
+			bodyInput: document.querySelector('#body'),
+			idInput: document.querySelector('#id'),
+			postSubmit: document.querySelector('.post-submit')
+		}
+	}
+}
+
+export const constants = new Constants();
