1309cba4a9ecf38144ad34fcec7affdd10f0af5d
eligarlo <eligarlo.developer@gmail.com>
2020-04-17 16:50:23 +0300

    added delete post method

--- src/app.js
+++ src/app.js
@@ -10,6 +10,9 @@ document.addEventListener('DOMContentLoaded', getPosts);
 // Listen for add post
 constants.DOMSelectors().postSubmit.addEventListener('click', submitPost);
 
+// Listen for delete post
+constants.DOMSelectors().post.addEventListener('click', deletePost);
+
 ///// End of event listeners /////
 
 ///// Start of methods /////
@@ -23,7 +26,7 @@ function getPosts() {
 }
 
 // Submit Post
-function submitPost(post) {
+function submitPost() {
 	const title = constants.DOMSelectors().titleInput.value;
 	const body = constants.DOMSelectors().bodyInput.value;
 	const apiPosts = constants.ApiUrl().posts;
@@ -46,6 +49,23 @@ function submitPost(post) {
 	} else {
 		ui.showAlert('All fields are required', 'alert alert-danger');
 	}
+}
+
+// Delete Post
+function deletePost(e) {
+	e.preventDefault();
+	if (e.target.parentElement.classList.contains('delete')) {
+		const id = e.target.parentElement.dataset.id;
+		if (confirm('Are you sure?')) {
+			http.delete(`${constants.ApiUrl().posts}/${id}`)
+			.then(data => {
+				ui.showAlert('Post Removed', 'alert alert-success');
+				getPosts();
+			})
+			.catch(err => console.log(err));
+		}
+	}
+	
 }
 
 ///// End of methods /////
