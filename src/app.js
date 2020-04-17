ee8f819258039b8b8b02dcbb1066a535c1e02dcc
eligarlo <eligarlo.developer@gmail.com>
2020-04-17 14:57:12 +0300

    submit post

--- src/app.js
+++ src/app.js
@@ -1,12 +1,51 @@
 import { http } from './modules/http';
 import { ui } from './modules/ui';
+import { constants } from './modules/constants';
 
+///// Start of event listeners /////
 
 // Get posts on Dom load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Listen for add post
+constants.DOMSelectors().postSubmit.addEventListener('click', submitPost);
+
+///// End of event listeners /////
+
+///// Start of methods /////
+
+// Get Posts
 function getPosts() {
-  http.get('http://localhost:3000/posts')
-  .then(data => ui.showPosts(data))
-  .catch(err => console.log(err))
+	const apiPosts = constants.ApiUrl().posts;
+	http.get(apiPosts)
+		.then(data => ui.showPosts(data))
+		.catch(err => console.log(err))
 }
+
+// Submit Post
+function submitPost(post) {
+	const title = constants.DOMSelectors().titleInput.value;
+	const body = constants.DOMSelectors().bodyInput.value;
+	const apiPosts = constants.ApiUrl().posts;
+
+	// Input validation
+	if (title && body && title.trim() !== '' && body.trim() !== '') {
+		const data = {
+			title,
+			body
+		}
+
+		// Create Post
+		http.post(apiPosts, data)
+			.then(data => {
+				ui.showAlert('Post added', 'alert alert-success');
+				ui.clearFields();
+				getPosts();
+			})
+			.catch(err => console.log(err))
+	} else {
+		ui.showAlert('All fields are required', 'alert alert-danger');
+	}
+}
+
+///// End of methods /////
--- src/modules/http.js
+++ src/modules/http.js
@@ -9,47 +9,47 @@
  **/
 
 class EasyHTTP {
-    // Make an HTTP GET Request
-    async get(url) {
-        const response = await fetch(url);
-        const resData = await response.json();
-        return resData;
-    }
+	// Make an HTTP GET Request
+	async get(url) {
+		const response = await fetch(url);
+		const resData = await response.json();
+		return resData;
+	}
 
-    // Make an HTTP POST Request
-    async post(url, data) {
-        const response = await fetch(url, {
-            method: "POST",
-            headers: { "Content-type": "application/json" },
-            body: JSON.stringify(data),
-        });
+	// Make an HTTP POST Request
+	async post(url, data) {
+		const response = await fetch(url, {
+			method: "POST",
+			headers: { "Content-type": "application/json" },
+			body: JSON.stringify(data),
+		});
 
-        const resData = await response.json();
-        return resData;
-    }
+		const resData = await response.json();
+		return resData;
+	}
 
-    // Make an HTTP PUT Request
-    async put(url, data) {
-        const response = await fetch(url, {
-            method: "PUT",
-            headers: { "Content-type": "application/json" },
-            body: JSON.stringify(data),
-        });
+	// Make an HTTP PUT Request
+	async put(url, data) {
+		const response = await fetch(url, {
+			method: "PUT",
+			headers: { "Content-type": "application/json" },
+			body: JSON.stringify(data),
+		});
 
-        const resData = await response.json();
-        return resData;
-    }
+		const resData = await response.json();
+		return resData;
+	}
 
-    // Make an HTTP DELETE Request
-    async delete(url) {
-        const response = await fetch(url, {
-            method: "DELETE",
-            headers: { "Content-type": "application/json" },
-        });
+	// Make an HTTP DELETE Request
+	async delete(url) {
+		const response = await fetch(url, {
+			method: "DELETE",
+			headers: { "Content-type": "application/json" },
+		});
 
-        const resData = await "Resource deleted...";
-        return resData;
-    }
+		const resData = await "Resource deleted...";
+		return resData;
+	}
 }
 
 export const http = new EasyHTTP();
